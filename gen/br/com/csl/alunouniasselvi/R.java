155cddc21f51b241b3706a54d1d07f54d9793501
Waldenilson Carneiro <wal.denilson@hotmail.com>
2015-02-12 01:23:42 -0300

    tela inicial.

--- gen/br/com/csl/alunouniasselvi/R.java
+++ gen/br/com/csl/alunouniasselvi/R.java
@@ -14,8 +14,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070001;
-        public static final int tv_listconfig=0x7f070000;
+        public static final int button1=0x7f070001;
+        public static final int button2=0x7f070002;
+        public static final int button3=0x7f070003;
+        public static final int imageView1=0x7f070000;
+        public static final int menu_settings=0x7f070005;
+        public static final int tv_listconfig=0x7f070004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -28,6 +32,9 @@ public final class R {
         public static final int ale_buscando_dados=0x7f040003;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int lb_creditos=0x7f040006;
+        public static final int lb_seminarios=0x7f040005;
+        public static final int lb_simulador_notas=0x7f040004;
         public static final int menu_settings=0x7f040002;
     }
     public static final class style {
