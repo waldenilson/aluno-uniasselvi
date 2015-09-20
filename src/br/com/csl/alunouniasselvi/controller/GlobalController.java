12e4782717d387dfa41d0af946282e81548acf1e
Waldenilson Carneiro <waldenilsonnovaes@gmail.com>
2015-09-20 11:36:37 -0300

    adicao do metodo de leitura do arquivo de banco de dados alunouniasselvisqlite.db.

--- src/br/com/csl/alunouniasselvi/controller/GlobalController.java
+++ src/br/com/csl/alunouniasselvi/controller/GlobalController.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Application;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
@@ -23,7 +24,8 @@ public class GlobalController extends Application {
 	private String keyAccess = "chave";
 	public String seminario = "[]";
 	private final String FILES = Environment.getExternalStorageDirectory()+"/AlunoUniasselvi/files/control/";
-		
+	private SQLiteDatabase database;
+	
 	public GlobalController(){
 		criarConfig();
 	}
@@ -148,4 +150,9 @@ public class GlobalController extends Application {
 	public void setKeyAccess(String keyAccess) {
 		this.keyAccess = keyAccess;
 	}	
+
+	public SQLiteDatabase getDatabase() {
+		return openOrCreateDatabase( FILES + "alunouniasselvisqlite.db", MODE_PRIVATE, null);
+	}
+
 }
